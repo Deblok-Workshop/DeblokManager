d3ee2178a0d0f4afb50a4224706f9a3f892fca4b
uhidontkno <hi@rare1k.dev>
2024-02-09 18:55:54 -0500

    boilerplate

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import { Elysia, error } from "elysia";
+const conffile = Bun.file("config/config.json")
+const config = JSON.parse(await conffile.text())
+
+let netaddr = '[::1]'
+netaddr = require('node:os').hostname()
+
+const server = new Elysia();
+
+console.log(`Listening on port ${config.webserver.port} or`),
+console.log(` │ 0.0.0.0:${config.webserver.port}`),
+console.log(` │ 127.0.0.1:${config.webserver.port}`),
+console.log(` │ ${netaddr}:${config.webserver.port}`),
+console.log(` └─────────────────────────>`),
+server.listen(config.webserver);
